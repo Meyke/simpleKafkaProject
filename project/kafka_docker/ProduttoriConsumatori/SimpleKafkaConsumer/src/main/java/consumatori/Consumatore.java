848b60212b4ef8850a2520eeb16fca2446d772a5
Meyke <michele.tedesco487@virgilio.it>
2018-06-07 18:24:29 +0200

    progetto kafka_docker

---
+++ project/kafka_docker/ProduttoriConsumatori/SimpleKafkaConsumer/src/main/java/consumatori/Consumatore.java
@@ -0,0 +1,51 @@
+package consumatori;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Consumatore {
+	
+	KafkaConsumer<String, String> consumer = null;
+	
+	public Consumatore(String topic, String group){
+		
+		Properties props = new Properties();
+	        props.put("bootstrap.servers", "10.11.1.73:9092");
+
+	        //group triciclo e palloncino
+	        props.put("group.id", group);
+	        
+	        //using auto commit
+	        props.put("enable.auto.commit", "true");
+	 
+	        //string inputs and outputs
+	        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	 
+	        //kafka consumer object
+	        consumer = new KafkaConsumer<String, String>(props);
+	 
+	        //subscribe to topic
+	        consumer.subscribe(Arrays.asList(topic));
+	}
+	
+	
+	
+	public List<String> receiveMessages(){
+			List<String> messages = new ArrayList<>();
+			//infinite poll loop
+	       ConsumerRecords<String, String> records = consumer.poll(100);
+	       for (ConsumerRecord<String, String> record : records){
+	           System.out.printf("topic = %s, partition = %s, offset = %d, key = %s, value = %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+	           messages.add(record.value());
+	       }
+	       return messages;
+	}
+
+}
---
+++ project/kafka_docker/generatoreTopic/kafka-configuration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# crate a topic named "topic" with a single partition and only one replica
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper 10.11.1.71:2181 --replication-factor 1 --partitions 2 --topic topic1
+
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper 10.11.1.71:2181 --replication-factor 1 --partitions 1 --topic topic2
+
+$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper 10.11.1.71:2181
+
+
