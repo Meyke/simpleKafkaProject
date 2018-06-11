c7aa62da9013a0b3c156064f1e514101d0c03cae
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2018-06-11 21:28:02 +0200

    Add files via upload
    Aggiunto wait-for-kafka
    Modificato il dockerfile affinchè faccia riferimento a wait-for-kafka e aggiunta dipendenza a telnet

---
+++ kafka_docker/ProduttoriConsumatori/SimpleKafkaProducer/wait-for-kafka.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# wait-for-kafka.sh
+
+set -e
+
+cmd="$@"
+while [[ $(echo "interrupt" | telnet 10.11.1.73 9092 2>&1 | grep "Connection refused" -c) == *1 ]]
+do
+	>&2 echo "Waiting for kafka to startup - sleeping"
+	sleep 1
+done
+echo "kafka started! running"
+sleep 20
+exec $cmd
+
