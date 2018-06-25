98d1979190d215fc089154b281667176d5241370
Meyke <michele.tedesco487@virgilio.it>
2018-06-25 12:39:36 +0200

    Delete restart-all-project.sh

--- ambienti/ambiente_swarm/restart-all-project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "################################################"
-echo "Stopping vagrant..."
-echo "################################################"
-vagrant halt
-./run-kafka-docker-project.sh
